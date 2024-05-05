515a1f79d5203bd4f427cb243b33dc9a85b93fa7
alexander-vashchuk <79473861+fordelord@users.noreply.github.com>
2024-05-05 17:06:34 +0300

    Add formula image

--- POP1/Form1.Designer.cs
+++ POP1/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace POP1
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.rInput = new System.Windows.Forms.NumericUpDown();
@@ -52,6 +53,7 @@ namespace POP1
             this.button1 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.rInput)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lInput)).BeginInit();
@@ -62,6 +64,7 @@ namespace POP1
             ((System.ComponentModel.ISupportInitialize)(this.mInput)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.aInput)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -302,11 +305,22 @@ namespace POP1
             this.textBox1.TabIndex = 4;
             this.textBox1.Text = "Частота коливань: ";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 301);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(929, 651);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.button1);
@@ -327,6 +341,7 @@ namespace POP1
             ((System.ComponentModel.ISupportInitialize)(this.mInput)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.aInput)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -353,6 +368,7 @@ namespace POP1
         private System.Windows.Forms.NumericUpDown mInput;
         private System.Windows.Forms.NumericUpDown rInput;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
